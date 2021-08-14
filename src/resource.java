a5fbb7dcf217fd9b0a2e33ac9fbd3fe9b2de0dbc
Matthew Oosthuyse <21784507@sun.ac.za>
2021-08-14 11:52:19 +0200

    added some methods to acces the number of resources that are available in the custer

--- src/resource.java
+++ src/resource.java
@@ -2,6 +2,7 @@ public class resource {
   private int id, numResources, quota;
   private char c;
   private int[][] clust;
+  private boolean hasRes[];
 
 
   public resource(int id, int numResources) {
@@ -24,6 +25,7 @@ public class resource {
 
   /**
    * Getting the cluster for this resource
+   *
    * @return 2D array of the cluster
    */
   public int[][] getCluser() {
@@ -32,20 +34,39 @@ public class resource {
 
   /**
    * Changes the resources that have been picked up by the ship
+   *
    * @param cluster - Which cluster the ship is at
-   * @param taken - The amount of resources the ship has taken.
+   * @param taken   - The amount of resources the ship has taken.
    */
   public void setResources(int cluster, int taken) {
     clust[cluster][3] -= taken;
+    if (clust[cluster][3] == 0) {
+      hasRes[cluster] = false;
+    }
+  }
+
+  /**
+   * Seeing how many resources are available in the cluster
+   * @param cluster  the cluster in question
+   * @return the number of resources available
+   */
+  public int availableResources(int cluster) {
+    return clust[cluster][3];
   }
 
 
-
+  /**
+   * sets the coordinates from the file for where the clusters are
+   *
+   * @param line The line of the coordinates of the clusters
+   */
   public void setCoords(String line) {
     // assigning the clusters here
     clust = new int[numResources][4];
+    hasRes = new boolean[numResources];
     line = line.substring(line.indexOf("|") + 1);
     for (int i = 0; i < numResources; i++) {
+      hasRes[i] = true;
       line = line.substring(line.indexOf(",") + 1);
       clust[i][0] = Integer.parseInt(line.substring(0, line.indexOf(",")));
       line = line.substring(line.indexOf(",") + 1);
