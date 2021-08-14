67c40515b8acc84a1f2bef3928ca7b2562039ccd
Matthew Oosthuyse <21784507@sun.ac.za>
2021-08-14 11:15:14 +0200

    Completed reading in the files

--- src/resource.java
+++ src/resource.java
@@ -1,9 +1,12 @@
 public class resource {
   private int id, numResources, quota;
+  private char c;
+  private int[][] clust;
 
 
   public resource(int id, int numResources) {
     this.id = id;
+    c = (char) (id + 96);
     this.numResources = numResources;
   }
 
@@ -21,5 +24,23 @@ public class resource {
 
   public void setResources(int taken) {
     this.numResources -= taken;
+  }
+
+  public void setCoords(String line) {
+    // assigning the clusters here
+    clust = new int[numResources][4];
+    line = line.substring(line.indexOf("|") + 1);
+    for (int i = 0; i < numResources; i++) {
+      line = line.substring(line.indexOf(",") + 1);
+      clust[i][0] = Integer.parseInt(line.substring(0, line.indexOf(",")));
+      line = line.substring(line.indexOf(",") + 1);
+      clust[i][1] = Integer.parseInt(line.substring(0, line.indexOf(",")));
+      line = line.substring(line.indexOf(",") + 1);
+      clust[i][2] = Integer.parseInt(line.substring(0, line.indexOf(",")));
+      line = line.substring(line.indexOf(",") + 1);
+      if (i != numResources - 1)
+        clust[i][3] = Integer.parseInt(line.substring(0, line.indexOf("|")));
+      else clust[i][3] = Integer.parseInt(line);
+    }
   }
 }
